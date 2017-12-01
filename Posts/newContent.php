8473637879232f2d566821b0496de859c493d3b0
ShayneSmither <34110331+ShayneSmither@users.noreply.github.com>
2017-12-01 16:42:46 -0600

    Update newContent.php
    updated server access credentials

--- Posts/newContent.php
+++ Posts/newContent.php
@@ -1,10 +1,10 @@
 <?php
 
 // variable declaration
-$servername = "sql303.epizy.com";
-$username = "epiz_20659244";
-$password = "rwShlqy4Tn";
-$dbname = "epiz_20659244_albumProject";
+$servername = "sql312.epizy.com";
+$username = "epiz_21149710";
+$password = "33803380";
+$dbname = "epiz_21149710_chat";
 // Create connection
 $db = new mysqli($servername, $username, $password, $dbname);
 
