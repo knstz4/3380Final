1b62d1129aa7a6deaa618cd0f724a422a2dff54c
Ben <benthrasher5@gmail.com>
2017-11-30 19:41:25 -0600

    Added newContent.php

---
+++ Posts/newContent.php
@@ -0,0 +1,31 @@
+<?php
+
+// variable declaration
+$servername = "sql303.epizy.com";
+$username = "epiz_20659244";
+$password = "rwShlqy4Tn";
+$dbname = "epiz_20659244_albumProject";
+// Create connection
+$db = new mysqli($servername, $username, $password, $dbname);
+
+//check for flag
+if isset($_POST['newContent']) {
+    //grab variables from post array
+    $body = mysqli_real_escape_string($db, $_POST['contentBody']);
+    $tag = mysqli_real_escape_string($db, $_POST['contentTag']);
+
+    //get user id from username
+    $username = $_SESSION['username'];
+    $userQuery = "SELECT id FROM users WHERE username = 'benthrasher5'";
+
+    $userResult = mysqli_query($db, $userQuery);
+    $userRow = $userResult->fetch_row();
+    $userId = $userRow["id"];
+
+    $query = "INSERT INTO posts (id, content, tag, uploadTime) VALUES ($userId, '$body', '$tag', now())";
+
+    mysqli_query($db, $query);
+}
+
+
+?>
