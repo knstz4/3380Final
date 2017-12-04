348e9ee1bfbc983f946fc317aa9074243d298032
Nereusss <xzznf@mail.missouri.edu>
2017-12-04 17:44:52 -0600

    Change the encrypted method
    Change md5 to password_hash and rearrange the code, add some comments

--- Login/server.php
+++ Login/server.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-// set up the credential
+// Define the database and info for later connection
 $servername = "sql312.epizy.com";
 $username = "epiz_21149710";
 $password = "33803380";
@@ -10,7 +10,7 @@ $dbname = "epiz_21149710_chat";
 // Create connection
 $db = new mysqli($servername, $username, $password, $dbname);
 
-// REGISTER USER
+// Register user
 if (isset($_POST['reg_user'])) {
 	// receive all input values from the form
 	$username = mysqli_real_escape_string($db, $_POST['username']);
@@ -20,10 +20,18 @@ if (isset($_POST['reg_user'])) {
 	
 	$errors =[];
 
-	// form validation: ensure that the form is correctly filled
-	if (empty($username)) { array_push($errors, "Username is required"); }
-	if (empty($email)) { array_push($errors, "Email is required"); }
-	if (empty($password_1)) { array_push($errors, "Password is required"); }
+	// Form validation: ensure that the form is correctly filled
+	if (empty($username)) { 
+		array_push($errors, "Username is required"); 
+	}
+
+	if (empty($email)) { 
+		array_push($errors, "Email is required"); 
+	}
+
+	if (empty($password_1)) { 
+		array_push($errors, "Password is required"); 
+	}
 
 	if ($password_1 != $password_2) {
 		array_push($errors, "The two passwords do not match");
@@ -37,9 +45,11 @@ if (isset($_POST['reg_user'])) {
            array_push($errors, "User already exists");
        }
 
-	// register user if there are no errors in the form
+	// Register user if there are no errors in the form
 	if (count($errors) == 0) {
-		$password = md5($password_1);//encrypt the password before saving in the database
+
+		// Use password_hash function to encrypt the user password
+		$password = password_hash($password_1, PASSWORD_DEFAULT);
 		$query = "INSERT INTO users (username, email, password) 
 				  VALUES('$username', '$email', '$password')";
 		mysqli_query($db, $query);
@@ -51,7 +61,7 @@ if (isset($_POST['reg_user'])) {
 
 }
 
-// LOGIN USER
+// Perform login process
 if (isset($_POST['login_user'])) {
 	$username = mysqli_real_escape_string($db, $_POST['username']);
 	$password = mysqli_real_escape_string($db, $_POST['password']);
@@ -61,20 +71,34 @@ if (isset($_POST['login_user'])) {
 	if (empty($username)) {
 		array_push($errors, "Username is required");
 	}
+	
 	if (empty($password)) {
 		array_push($errors, "Password is required");
 	}
 
 	if (count($errors) == 0) {
-		$password = md5($password);
-		$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-		$results = mysqli_query($db, $query);
 
-		if (mysqli_num_rows($results) == 1) {
-			$_SESSION['username'] = $username;
-			$_SESSION['success'] = "You are now logged in";
-			header('location: index.php');
-		}else {
+		$query = "SELECT * FROM users WHERE username = '$username'";
+
+		$result = mysqli_query($db, $query);
+
+   		$row = mysqli_fetch_assoc($result);
+
+  	    $password_data = $row['password'];
+		
+		if (password_verify($password, $password_data)) {
+			
+			$query = "SELECT * FROM users WHERE username='$username'";
+			$results = mysqli_query($db, $query);
+		
+			if (mysqli_num_rows($results) == 1) {
+				$_SESSION['username'] = $username;
+				$_SESSION['success'] = "You are now logged in";
+				header('location: index.php');
+			}
+		}
+
+		else {
 			array_push($errors, "Wrong username/password combination");
 		}
 	}
