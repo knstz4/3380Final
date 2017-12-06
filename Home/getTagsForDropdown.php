15ec43842c11210084bc2983814399fbcef95033
Ben <benthrasher5@gmail.com>
2017-12-06 17:18:14 -0600

    added getPosts and getTags

---
+++ Home/getTagsForDropdown.php
@@ -0,0 +1,15 @@
+<?php
+
+  require_once "../Posts/dbConnect.php";
+
+  $query = "SELECT DISTINCT tag FROM posts";
+
+  $result = mysqli_query($db, $query);
+
+  while ($row = mysqli_fetch_assoc($result)) {
+    $tag = $row["tag"];
+
+    echo "<li><a href='#'>$tag</a></li>";
+  }
+
+?>
