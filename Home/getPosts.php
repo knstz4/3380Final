41eaa466fac09d6abd824c04b4f73c8d03a50ac6
Ben <benthrasher5@gmail.com>
2017-12-06 17:46:36 -0600

    edit/delete only show for you, updated index.php to latest

---
+++ Home/getPosts.php
@@ -0,0 +1,53 @@
+<?php
+
+  session_start();
+
+  require_once "../Posts/dbConnect.php";
+
+  $query = "SELECT * FROM posts";
+
+  if(isset($_GET["tag"])) {
+    $query .= " WHERE tag = '" . $_GET["tag"] . "'";
+  }
+
+  //sort by datetime
+  $query .= " ORDER BY uploadTime DESC";
+
+  $result = mysqli_query($db, $query);
+
+  while ($row = mysqli_fetch_assoc($result)) {
+    $id = $row["contentId"];
+    $userId = $row["id"];
+    $content = $row["content"];
+    $tag = $row["tag"];
+    $uploadTime = $row["uploadTime"];
+
+    //get username from user id
+    $userQuery = "SELECT username FROM users WHERE id = $userId";
+    $userResult = mysqli_query($db, $userQuery);
+    $userRow = mysqli_fetch_assoc($userResult);
+    $username = $userRow["username"];
+
+    //format date string
+    $date = date_create_from_format('Y-m-d H:i:s', $uploadTime);
+    $dateString = "Posted on " . $date->format('F jS, Y') . " at " . $date->format('g:i a');
+
+    //build html
+    echo "<div class='media'>";
+    echo "<div class='media-body'>";
+    echo "<h4 class='media-heading'>$username</h4>";
+    echo "<small><i></i>$dateString - <a href='?tag=$tag'>$tag</a></small>";
+    echo "<p>$content</p>";
+
+    //only allow edit/delete if its your post
+    if($_SESSION["username"] == $username) {
+      echo "<div class='btn-group'>";
+      echo "<button type='button' class='btn btn-primary btn-xs'>Edit</button>";
+      echo "<button type='button' class='btn btn-primary btn-xs'>Delete</button>";
+      echo "</div>";
+    }
+
+    echo "</div></div>";
+  }
+
+?>
--- Login/index.php
+++ Login/index.php
@@ -1,5 +1,5 @@
-<?php 
-	session_start(); 
+<?php
+	session_start();
 
 	if (!isset($_SESSION['username'])) {
 		$_SESSION['msg'] = "You must log in first";
@@ -14,35 +14,95 @@
 
 ?>
 <!DOCTYPE html>
+
+
 <html>
+
 <head>
-	<title>Home</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+     <!-- Latest compiled and minified CSS -->
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+     <title>Chatta</title>
+
+     <script>
+
+          function submitPost() {
+
+               var output;
+               output = document.getElementById("message").innerHTML;
+               tag = document.getElementById("tag");
+
+               document.getElementById("output") = output;
+               document.getElementById("message") = "";
+          }
+
+
+     </script>
 </head>
+
 <body>
-	<div class="header">
-		<h2>Home Page</h2>
-	</div>
-	<div class="content">
+     <div>
+          <h1>Welcome to Chatta!</h1>
+     </div>
 
-		<!-- notification message -->
-		<?php if (isset($_SESSION['success'])) : ?>
-			<div class="error success" >
-				<h3>
-					<?php 
-						echo $_SESSION['success']; 
-						unset($_SESSION['success']);
-					?>
-				</h3>
-			</div>
-		<?php endif ?>
+     <div class = "dropdown">
+          <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Discussion Board
+          <span class="caret"></span></button>
+          <ul class="dropdown-menu">
+						<?php require_once "getTagsForDropdown.php"; ?>
+          </ul>
+     </div>
 
-		<!-- logged in user information -->
-		<?php  if (isset($_SESSION['username'])) : ?>
-			<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
-			<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
-		<?php endif ?>
-	</div>
-		
+     <div class ="jumbotron" id="output">
+			 	<?php require_once "getPosts.php"; ?>
+		 </div>
+
+     <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">New Post</button>
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal">
+    <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">New Message</h4>
+        </div>
+        <div class="modal-body">
+        	<form method="post" action = "../Posts/newContent.php">
+              	<div class="form-group">
+               		<label>Message</label>
+               		<input type="text" class="form-control" name ="contentBody">
+          		</div>
+         	 <div class="form-group">
+               <label>Tag</label>
+               <input type ="text" class="form-control" name ="contentTag">
+          	</div>
+		 <button type="submit" name="newContent" class="btn btn-default">Submit</button>
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</form>
+
+
+
+
+
+
 </body>
+
+
 </html>
